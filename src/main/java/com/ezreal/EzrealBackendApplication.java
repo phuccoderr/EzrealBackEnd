cf60462dc99cf591e3c3de41819b20a17cd2343d
phuccoderr <phuctapcode@gmail.com>
2024-04-24 20:06:42 +0700

    Upload Main Backend

---
+++ src/main/java/com/ezreal/EzrealBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ezreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzrealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzrealBackendApplication.class, args);
+	}
+
+}
